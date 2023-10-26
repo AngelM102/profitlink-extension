61c927cfd478d7dee9372aa6786ef1da703df6e2
Angelop_ <morillo0210angel@gmail.com>
2023-10-26 09:42:22 -0400

    .

--- content.js
+++ content.js
@@ -1,17 +1,22 @@
-function createInput(id, value) {
-    const hiddenField = document.createElement("input");
-    hiddenField.type = "hidden";
-    hiddenField.id = id;
-    hiddenField.value = value;
-    document.body.appendChild(hiddenField);
+function createInputs() {
+    const div = document.createElement("div"); // create a new div element
+    div.style.display = 'none'; // hide the div element
+    document.body.appendChild(div); // append the div element to the document body
+
+    createInput(div, "iplocal", "ip"); // create an input element with id "iplocal" and value "ip", and append it to the div
+    createInput(div, "impresoraFiscal", "impresora"); // create an input element with id "impresoraFiscal" and value "impresora", and append it to the div
+    createInput(div, "modeloImpresora", "modelo"); // create an input element with id "modeloImpresora" and value "modelo", and append it to the div
+    createInput(div, "flag", "flag"); // create an input element with id "flag" and value "flag", and append it to the div
 }
 
-function createInputs() {
-    createInput("iplocal", "ip");
-    createInput("impresoraFiscal", "impresora");
-    createInput("modeloImpresora", "modelo");
-    createInput("puertoCom", "puerto");
+function createInput(parent, id, value) {
+    const hiddenField = document.createElement("input"); // create a new input element
+    hiddenField.type = "hidden"; // set its type to "hidden"
+    hiddenField.id = id; // set its id to the given id
+    hiddenField.value = value; // set its value to the given value
+    parent.appendChild(hiddenField); // append the input element to the parent element
 }
+
 
 function updateInput(id, key) {
     chrome.storage.sync.get(key, function (result) {
@@ -23,7 +28,7 @@ function updateInputs() {
     updateInput('iplocal', 'iplocal');
     updateInput('impresoraFiscal', 'impresoraFiscal');
     updateInput('modeloImpresora', 'modeloImpresora');
-    updateInput('puertoCom', 'puertoCom');
+    updateInput('flag', 'flag');
 }
 
 createInputs();
--- popup.js
+++ popup.js
@@ -1,4 +1,60 @@
-let configuracion = {}
+let configuracion = {
+    impresoraFiscales: [
+        { value: 'factory', text: 'The Factory' },
+        { value: 'pnp', text: 'Desarrolos PNP' },
+    ],
+    modelos: [
+        'TM-T20II',
+        'TM-T88V',
+        'TM-T88VI',
+        'TM-T88VI-iHUB',
+        'TM-T88VI-DT2',
+        'TM-T88VI-iHUB-DT2',
+    ],
+    flags: [
+        { value: '00', text: '00 - (8 enteros y 2 decimales)' },
+        { value: '01', text: '01 - (7 enteros y 3 decimales)' },
+        { value: '02', text: '02 - (6 enteros y 4 decimales)' },
+        { value: '11', text: '11 - (9 enteros y 1 decimal)' },
+        { value: '12', text: '12 - (10 enteros y 0 decimales)' },
+        { value: '30', text: '30 - (14 enteros y 2 decimales)' },
+    ]
+}
+
+
+function createSelect(id, options, label) {
+    const $div = document.createElement("div");
+    const $label = document.createElement("label");
+    const select = document.createElement("select");
+
+
+    $div.classList.add("form-group");
+    select.id = id;
+    select.style.width = '100%';
+
+
+    options.forEach(option => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option.value;
+        optionElement.text = option.text;
+        select.appendChild(optionElement);
+    });
+
+
+    $label.appendChild(document.createTextNode(label));
+    $div.appendChild($label);
+    $div.appendChild(select);
+    document.getElementById('data-selects').appendChild($div);
+}
+
+
+function createSelects() {
+    createSelect('impresoraFiscal', configuracion.impresoraFiscales, 'Impresora Fiscal');
+    createSelect('modeloImpresora', configuracion.modelos.map(modelo => ({ value: modelo, text: modelo })), 'Modelo Impresora');
+    createSelect('flag', configuracion.flags, 'Flag 21');
+}
+
+
 
 
 
@@ -15,8 +71,7 @@ function getUserIP() { //  onNewIp - your listener function for new IPs
 
     function iterateIP(ip) {
         if (!localIPs[ip]) {
-            configuracion.iplocal = ip
-            chrome.storage.sync.set({ 'iplocal': configuracion.iplocal });
+            chrome.storage.sync.set({ 'iplocal': ip });
         }
         localIPs[ip] = true;
     }
@@ -45,37 +100,51 @@ function getUserIP() { //  onNewIp - your listener function for new IPs
 
 
 function setValueInput(id, value) {
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
             { code: `document.getElementById("${id}").value = "${value}"` }
         );
     });
+
 }
+
+
+
 
 function saveConfig() {
     const impresoraFiscal = document.getElementById('impresoraFiscal').value;
     const modeloImpresora = document.getElementById('modeloImpresora').value;
-    const puertoCom = document.getElementById('puertoCom').value;
+    const flag = document.getElementById('flag').value;
 
-    chrome.storage.sync.set({ impresoraFiscal, modeloImpresora, puertoCom }, function () {
+
+    chrome.storage.sync.set({ impresoraFiscal, modeloImpresora, flag }, function () {
         setValueInput('impresoraFiscal', impresoraFiscal);
         setValueInput('modeloImpresora', modeloImpresora);
-        setValueInput('puertoCom', puertoCom);
+        setValueInput('flag', flag);
     });
 }
+
+
+
 
 async function loadConfig() {
     await getUserIP()
-    chrome.storage.sync.get(['iplocal', 'impresoraFiscal', 'modeloImpresora', 'puertoCom'], function (result) {
+
+    chrome.storage.sync.get(['iplocal', 'impresoraFiscal', 'modeloImpresora', 'flag'], function (result) {
         document.getElementById('ip').value = result.iplocal;
         document.getElementById('impresoraFiscal').value = result.impresoraFiscal;
         document.getElementById('modeloImpresora').value = result.modeloImpresora;
-        document.getElementById('puertoCom').value = result.puertoCom;
+        document.getElementById('flag').value = result.flag;
     });
 }
 
+
+
 if (document.getElementById('ip') !== null) {
-    document.getElementById('guardar').addEventListener('click', saveConfig);
+    createSelects();
     loadConfig();
+
+    document.getElementById('guardar').addEventListener('click', saveConfig);
 }
